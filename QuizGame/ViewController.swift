cbc0693a0d12be739f1b2d8cab99d14ea03440d0
Ville-Matti Hakanpää <villematti@villematti.info>
2020-05-19 10:07:58 +0300

    Initial Commit

---
+++ QuizGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizGame
+//
+//  Created by Ville-Matti Hakanpää on 19.5.2020.
+//  Copyright © 2020 Ville-Matti Hakanpää. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
